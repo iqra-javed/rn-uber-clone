1ba17d68eb9abd4e419910f4d26334d51f994e0c
iqra-javed <35740252+iqra-javed@users.noreply.github.com>
2021-09-26 22:04:26 -0500

    Start working on MapScreen, set up React Native Navigation and Google Places Autocomplete

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,19 +1,60 @@
-import React from 'react'
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
+import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from '@env';
+import { useDispatch } from 'react-redux';
+import { setDestination, setOrigin } from '..slices/navSlice';
 
 const HomeScreen = () => {
-    return (
-        <SafeAreaView>
-            <Text style={styles.text}>I am the home screen</Text>
-        </SafeAreaView>
-    )
-}
+  const dispatch = useDispatch();
+  return (
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: 'contain',
+          }}
+          source={{
+            uri: 'https://links.papareact.com/gzs',
+          }}
+        />
+        <GooglePlacesAutocomplete
+          nearbyPlacesAPI='GooglePlacesSearch'
+          debounce={400}
+          placeholder='Where From?'
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          onPress={(data, details = null) => {
+            // 'details' is provided when fetchDetails = true
+            console.log(data, details);
+          }}
+          fetchDetails={true}
+          //   returnKeyType={'search'}
+          enablePoweredByContainer={false}
+          minLength={2}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: 'en',
+          }}
+        />
+        <NavOptions />
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
 const styles = StyleSheet.create({
-    text: {
-
-    }
-})
+  text: {},
+});
