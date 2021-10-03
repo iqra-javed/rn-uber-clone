ad511c0d77e1ab390297bfacecc9c23629ed8ea2
iqra-javed <35740252+iqra-javed@users.noreply.github.com>
2021-10-03 17:16:18 -0500

    Install and display react-native-maps

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -3,6 +3,8 @@ import { FlatList, Text, TouchableOpacity, View, Image } from 'react-native';
 import { Icon } from 'react-native-elements';
 import tw from 'tailwind-react-native-classnames';
 import { useNavigation } from '@react-navigation/native';
+import { selectOrigin } from '../slices/navSlice';
+import { useSelector } from 'react-redux';
 
 const data = [
   {
@@ -20,6 +22,7 @@ const data = [
 ];
 const NavOptions = () => {
   const navigation = useNavigation();
+  const origin = useSelector(selectOrigin);
 
   return (
     <FlatList
@@ -30,8 +33,9 @@ const NavOptions = () => {
         <TouchableOpacity
           onPress={() => navigation.navigate(item.screen)}
           style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+          disabled={!origin}
         >
-          <View>
+          <View style={tw`${!origin && 'opacity-20'}`}>
             <Image
               style={{ width: 120, height: 120, resizeMode: 'contain' }}
               source={{
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,7 +5,7 @@ import NavOptions from '../components/NavOptions';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { GOOGLE_MAPS_APIKEY } from '@env';
 import { useDispatch } from 'react-redux';
-import { setDestination, setOrigin } from '..slices/navSlice';
+import { setDestination, setOrigin } from '../slices/navSlice';
 
 const HomeScreen = () => {
   const dispatch = useDispatch();
@@ -36,10 +36,16 @@ const HomeScreen = () => {
           }}
           onPress={(data, details = null) => {
             // 'details' is provided when fetchDetails = true
-            console.log(data, details);
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
+            dispatch(setDestination(null));
           }}
           fetchDetails={true}
-          //   returnKeyType={'search'}
+          returnKeyType={'search'}
           enablePoweredByContainer={false}
           minLength={2}
           query={{
